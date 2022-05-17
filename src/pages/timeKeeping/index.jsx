2a6c29be3587c0283430676f1e5bbec5bf153dd9
longtc9c2000 <65326062+longtc9c2000@users.noreply.github.com>
2022-05-17 13:24:32 +0700

    keeping

--- src/pages/timeKeeping/index.jsx
+++ src/pages/timeKeeping/index.jsx
@@ -1,7 +1,84 @@
 import React from 'react';
+import { Typography, PageHeader, Dropdown, Space, DatePicker } from 'antd';
+import HistoryTable from './historyTable/index';
+import './styles.scss';
+import DownOutline from './icon/downOutline';
 
+const { Title } = Typography;
+
+const today = new Date();
+const formatMonth = (date,format) =>{
+  const map = {
+    mm: date.getMonth() + 1,
+    yyy: date.getFullYear()
+  }
+  return format.replace(/mm|yyy/gi, matched => map[matched])
+}
+const month=formatMonth(today,'mm/yyy')
+console.log(formatMonth(today,'mm/yyy'))
+const monthFormat = ' MM/YYYY';
+const iconDown = <DownOutline/>
 function TimeKeeping(props) {
-  return <div>TimeKeeping</div>;
+  return (
+    <>
+      <PageHeader style={{ padding: '0' }}>
+        <Title style={{ padding: '0px 19px' }} level={2}>
+          Lịch sử chấm công
+          <Title
+            style={{
+              display: 'inline',
+              color: '#066F9B',
+              marginLeft: '0.8rem',
+            }}
+            level={2}
+          >
+            Tháng{' '}
+            <DatePicker
+              suffixIcon = {iconDown}
+              placeholder={month}
+              format={monthFormat}
+              bordered={'none'}
+              picker="month"
+              allowClear={false}
+            />
+          </Title>
+        </Title>
+        <Title style={{ padding: '0px 54px', marginTop: '0' }} level={3}>
+          Tổng số công
+          <Title
+            style={{
+              display: 'inline',
+              color: '#066F9B',
+              marginLeft: '7.7rem',
+            }}
+            level={3}
+          >
+            7,7/20
+          </Title>
+        </Title>
+
+        <Dropdown>
+          <a>
+            <Space
+              style={{
+                fontSize: '20px',
+                color: '#066F9B',
+                fontWeight: 'bold',
+                float: 'right',
+                marginTop: '-39px',
+                paddingRight: '10px',
+              }}
+            >
+              Filter
+              <DownOutline />
+            </Space>
+          </a>
+        </Dropdown>
+      </PageHeader>
+      <HistoryTable />
+      <Space></Space>
+    </>
+  );
 }
 
 export default TimeKeeping;
