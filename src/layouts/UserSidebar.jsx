19718f75ad10c76f69a4a9ea445e2363d449350d
dieptx@itop.io <manhkien1998@gmail.com>
2022-06-08 14:06:12 +0700

    test

--- src/layouts/UserSidebar.jsx
+++ src/layouts/UserSidebar.jsx
@@ -2,13 +2,14 @@ import { Menu } from 'antd';
 import React from 'react';
 import { useTranslation } from 'react-i18next';
 import { Link } from 'react-router-dom';
+import CalendarIcon from '../components/icons/CalendarIcon';
 import SendIcon from '../components/icons/SendIcon';
 
 function UserSidebar(props) {
   const { t } = useTranslation();
   return (
     <>
-      <Menu.Item key="/time_keeping" icon={<SendIcon />}>
+      <Menu.Item key="/time_keeping" icon={<CalendarIcon />}>
         <Link to="/time_keeping">{t(`sidebar.time_keeping`)}</Link>
       </Menu.Item>
       <Menu.Item key="/request" icon={<SendIcon />}>
